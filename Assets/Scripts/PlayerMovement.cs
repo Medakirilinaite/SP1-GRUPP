47ab84bd6ab263814cdc2e084344ffad063cb49a
Calle L <callelindqvist@hotmail.com>
2026-09-17 11:17:12 +0200

    Fixat walljump och movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,15 +9,18 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float maxSpeed = 5f;
     [SerializeField] private float acceleration = 30f;
-    [SerializeField] private float deceleration = 40f;
     [SerializeField] private float jumpForce = 200f;
-    [SerializeField] private Transform leftFoot, rightFoot;
+    [SerializeField] private Transform leftFoot, rightFoot, rightHand, leftHand;
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private float raycastDistance = 0.25f;
     [SerializeField] private AudioClip[] jumpSounds;
     [SerializeField] private ParticleSystem jumpParticleSystem;
+
+    [SerializeField] private ParticleSystem doubleJumpParticleSystem;
+
     bool canMove = true;
 
+    private bool isOnWallBool;
     private AudioSource audioSource;
     private Rigidbody2D rgbd;
     private SpriteRenderer rend;
@@ -41,6 +44,8 @@ public class PlayerMovement : MonoBehaviour
     {
         moveDirection = move.action.ReadValue<float>();
 
+        isOnWall();
+
         anim.SetFloat("MoveSpeed",Mathf.Abs (rgbd.linearVelocity.x));
         anim.SetFloat("VerticalSpeed", rgbd.linearVelocity.y);
         anim.SetBool("IsGrounded", CheckIsGrounded());
@@ -54,7 +59,22 @@ public class PlayerMovement : MonoBehaviour
        {
             FlipSprite(false);
        }
-    }
+
+        if (isOnWallBool && !CheckIsGrounded() && moveDirection != 0)
+        {
+
+            float newSpeed = Mathf.MoveTowards(rgbd.linearVelocity.y, 0, 40 * Time.fixedDeltaTime);
+            rgbd.linearVelocity = new Vector2(rgbd.linearVelocity.x, newSpeed);
+            
+           // rgbd.mass = 0.7f;
+            rgbd.gravityScale = 1f;
+        }
+        else {
+            //rgbd.mass = 1f;
+            rgbd.gravityScale = 2;
+                };
+
+        }
 
     private void FixedUpdate()
     {
@@ -64,20 +84,10 @@ public class PlayerMovement : MonoBehaviour
         }
         //rgbd.linearVelocity = new Vector2(moveDirection * moveSpeed * Time.deltaTime, rgbd.linearVelocity.y);
 
+        //Targetspeed blir 0 om man inte håller på någon knapp, och 1* maxSpeed om man gör det
         float targetSpeed = moveDirection * maxSpeed;
 
-        float speedChange;
-
-        if (moveDirection != 0)
-        {
-            speedChange = acceleration;
-        }
-        else
-        {
-            speedChange = deceleration;
-        }
-
-        float newSpeed = Mathf.MoveTowards(rgbd.linearVelocity.x,targetSpeed,speedChange * Time.fixedDeltaTime);
+        float newSpeed = Mathf.MoveTowards(rgbd.linearVelocity.x,targetSpeed, acceleration * Time.fixedDeltaTime);
 
         rgbd.linearVelocity = new Vector2(newSpeed,rgbd.linearVelocity.y);
     }
@@ -101,21 +111,44 @@ public class PlayerMovement : MonoBehaviour
 
             rgbd.AddForce(new Vector2(0, jumpForce));
             jumpParticleSystem.Play();
-            int randomJumpSound = Random.Range(0,jumpSounds.Length);
+            int randomJumpSound = Random.Range(0, jumpSounds.Length);
             audioSource.PlayOneShot(jumpSounds[randomJumpSound]);
-        } else if (canDoubleJump == true)
+        } else if (isOnWallBool== true) {
+
+            rgbd.AddForce(new Vector2(jumpForce*0.75f, jumpForce*0.5f));
+            jumpParticleSystem.Play();
+            int randomJumpSound = Random.Range(0, jumpSounds.Length);
+            audioSource.PlayOneShot(jumpSounds[randomJumpSound]);
+        }
+        else if (canDoubleJump == true)
         {
             //resettar ens momentum i Y-led så man inte kan stacka massa momentum
             rgbd.linearVelocity = new Vector2(rgbd.linearVelocityX, 0);
 
             rgbd.AddForce(new Vector2(0, jumpForce));
-            jumpParticleSystem.Play();
+            doubleJumpParticleSystem.Play();
             int randomJumpSound = Random.Range(0, jumpSounds.Length);
             audioSource.PlayOneShot(jumpSounds[randomJumpSound]);
             canDoubleJump = false;
         }
     }
+    private void isOnWall()
+    {
+        RaycastHit2D leftHandHit = Physics2D.Raycast(leftHand.position, Vector2.left, 0.05f, whatIsGround);
+        RaycastHit2D rightHandHit = Physics2D.Raycast(rightHand.position, Vector2.right, 0.05f, whatIsGround);
 
+        if (leftHandHit.collider != null && leftHandHit || rightHandHit.collider != null && rightHandHit)
+        {
+            print("Händer tar på nåt");
+            isOnWallBool = true;
+        }
+        else
+        {
+            print("Händer tar inte på nåt");
+            isOnWallBool = false;
+
+        }
+    }
     private bool CheckIsGrounded()
 
     {
@@ -124,6 +157,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (leftHit.collider != null && leftHit || rightHit.collider != null && rightHit)
         {
+
             EnableDoubleJump();
             return true;
         }
@@ -131,6 +165,7 @@ public class PlayerMovement : MonoBehaviour
         {
             return false;
         }
+
     }
 
     public void TakeKnockback(float knockbackForce, float upwardsForce)
