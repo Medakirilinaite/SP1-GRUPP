3648d9a22ffdf1a148109204b8543d14e4b9c023
allanisse <aloismail@icloud.com>
2026-09-17 11:48:45 +0200

    MUSIK
    ÄNDRAT

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -134,8 +134,8 @@ public class PlayerMovement : MonoBehaviour
     }
     private void isOnWall()
     {
-        RaycastHit2D leftHandHit = Physics2D.Raycast(leftHand.position, Vector2.left, 0.3f, whatIsGround);
-        RaycastHit2D rightHandHit = Physics2D.Raycast(rightHand.position, Vector2.right, 0.3f, whatIsGround);
+        RaycastHit2D leftHandHit = Physics2D.Raycast(leftHand.position, Vector2.left, 0.15f, whatIsGround);
+        RaycastHit2D rightHandHit = Physics2D.Raycast(rightHand.position, Vector2.right, 0.15f, whatIsGround);
         print("Är i isOnWall");
         if (leftHandHit.collider != null && leftHandHit || rightHandHit.collider != null && rightHandHit)
         {
