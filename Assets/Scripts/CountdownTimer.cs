df7864c6ebd5d63d00180c46902c140a6ce9d6e9
Calle L <callelindqvist@hotmail.com>
2026-09-21 12:00:40 +0200

    Timer

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -8,37 +8,50 @@ public class CountdownTimer : MonoBehaviour
 
     [SerializeField] private float timeLeft;
 
+    private GameObject startZone;
     private Light2D myLight;
 
-
+    public bool startTimer = false;
     private int levelIndex = 3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         myLight = GetComponent<Light2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeLeft -= Time.deltaTime;
-        if (timeLeft < 0)
+
+        if(startTimer == true)
         {
-            LoadNextLevel();
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                LoadNextLevel();
+            }
+
+            if (timeLeft < 10)
+            {
+                //print("under 10 sek");
+                setLightLevel(timeLeft / 10);
+            }
+
+            //timeText.text = ""+timeLeft;
+            timeText.SetText("{0:0}", timeLeft);
         }
 
-        if (timeLeft<10)
-        {
-            print("under 10 sek");
-            setLightLevel(timeLeft/10);
-        }
-
-        //timeText.text = ""+timeLeft;
-        timeText.SetText("{0:0}", timeLeft);
 
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            print("Player has entered starting zone");
+            startTimer = true;
+        }
+    }
     private void LoadNextLevel()
     {
         SceneManager.LoadScene(levelIndex);
